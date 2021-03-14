694530a4d3de1dea0d862e30cab521ffe92fc7cd
timothy <wangsz2015@foxmail.com>
2021-03-14 21:32:53 +0800

    a little change

--- 浦发机考编程题/51.两数之和.java
+++ 浦发机考编程题/51.两数之和.java
@@ -7,6 +7,21 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         // 需要熟练掌握java版哈希表的使用
+        // Map<Integer, Integer>
+         Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        for (int i=0; i<nums.length; i++){
+            // containsKey(Object key) 是否存在特定的key
+            if (mp.containsKey(target - nums[i])){
+                res[0] = i;
+                res[1] = mp.get(target - nums[i]);
+                break;
+            }
+            // mp[nums[i]] = i;
+            mp.put(nums[i], i);
+        }
+
+        // Map<String, Integer>
+        /*
         Map<String, Integer> mp = new HashMap<String, Integer>();
         for (int i=0; i<nums.length; i++){
             // containsKey(Object key) 是否存在特定的key
@@ -18,6 +33,8 @@ class Solution {
             // mp[nums[i]] = i;
             mp.put(String.valueOf(nums[i]), i);
         }
+        */
+
         return res;
     }
 }
